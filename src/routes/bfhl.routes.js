7d49039e3420be246559f428c206de977eac6fd5
Manish7291 <manishkau6239@gmail.com>
2026-02-10 13:07:06 +0530

    Add GET /bfhl route

--- src/routes/bfhl.routes.js
+++ src/routes/bfhl.routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const bfhlController = require('../controllers/bfhl.controller');
 
+router.get('/', (req, res) => {
+    res.status(200).json({ operation_code: 1 });
+});
+
 router.post('/', bfhlController.handleBfhl);
 
 module.exports = router;
