11ce1c02dc30a3d80c34dd9fd2182a6a5385446f
Manish7291 <manishkau6239@gmail.com>
2026-02-10 13:10:52 +0530

    Revert GET route

--- src/routes/bfhl.routes.js
+++ src/routes/bfhl.routes.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bfhlController = require('../controllers/bfhl.controller');
 
-router.get('/', (req, res) => {
-    res.status(200).json({ operation_code: 1 });
-});
-
 router.post('/', bfhlController.handleBfhl);
 
 module.exports = router;
